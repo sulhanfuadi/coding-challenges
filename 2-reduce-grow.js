1aa824b80e2ce43fbb42db7605004becf07b45b9
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-01-24 21:38:09 +0700

    day 02

---
+++ 2-reduce-grow.js
@@ -0,0 +1,23 @@
+// Coding Everyday: Reduce but Grow
+// January 24, 2024
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+// Solution #01
+// function grow(x){
+//     let result = x[0];
+//     for (let i = 1; i < x.length; i++){
+//         result *= x[i];
+//     }
+//     return result;
+// }
+
+// Solution #02
+// function grow(x){
+//     return x.reduce((acc, curr) => acc * curr, 1);
+// }
+
+// Solution #03
+const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+
+// test: must be returned 16
+console.log(grow([4, 1, 1, 1, 4]));
