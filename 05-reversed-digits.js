f794ba980aa96261e39c1b228173e32db6dc9ebe
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-01-28 09:40:35 +0700

    add new kata

---
+++ 05-reversed-digits.js
@@ -0,0 +1,21 @@
+// Coding Everyday: Convert number to reversed array of digits
+// January 28, 2024
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+
+// Solution #01
+// function digitize(n) {
+//     const nString = n.toString();
+//     const nArray = nString.split('').reverse();
+//     return nArray.map(nString => parseInt(nString))
+// }
+
+// Solution #02
+// function digitize(n) {
+//   return String(n).split('').map(Number).reverse();
+// }
+
+// Solution #03
+const digitize = n => n.toString().split('').reverse().map(nString => parseInt(nString));
+
+// test: must be returned [1, 3, 2, 5, 3]
+console.log(digitize(35231));
