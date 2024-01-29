279efe6003a93ec6e06397b3dc0aefa696356bc7
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-01-29 09:56:48 +0700

    add new katas

---
+++ 07-multiply.js
@@ -0,0 +1,14 @@
+// Coding Everyday: Multiply
+// January 29, 2024
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+// Solution #01
+// function multiply(a, b){
+//   return a * b;
+// }
+
+// Solution #02
+const multiply = (a, b) => a * b;
+
+// test: must be returned 15
+console.log(multiply(3,5));
