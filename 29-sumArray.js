b00592377770c013b42362fb1cd5a3af08d4e117
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-02-14 17:59:15 +0700

    add new kata

--- 29-sumArray.js
+++ 29-sumArray.js
@@ -3,15 +3,27 @@
 // https://www.codewars.com/kata/576b93db1129fcf2200001e6
 
 // Solution #01
-
+// function sumArray(array) {
+//     if (array == null || array.length <= 2) return 0;
+//     const sorted = array.sort((a, b) => a - b);
+//     let result = 0;
+//     for (let i = 1; i < sorted.length - 1; i++) {
+//         result += sorted[i];
+//     }
+//     return result;
+// }
 
 // Solution #02
-
+// function sumArray(array) {
+//     if (array == null || array.length <= 2) return 0;
+//     return array.sort((a, b) => a - b).slice(1, -1).reduce((acc, crr) => acc + crr);
+// }
 
 // Solution #03
+// const sumArray = (array) => (array == null || array.length <= 2)? 0 : array.sort((a, b) => a - b).slice(1, -1).reduce((acc, crr) => acc + crr);
 
 // Solution #04
-// const getGrade = (...s) => ((s.reduce((acc, curr) => acc + curr) / s.length) < 60)? 'F': ((s.reduce((acc, curr) => acc + curr) / s.length) < 70)? 'D' : ((s.reduce((acc, curr) => acc + curr) / s.length) < 80)? 'C' : ((s.reduce((acc, curr) => acc + curr) / s.length) < 90)? 'B': 'A';
+const sumArray = (array) => array? array.sort((a, b) => a - b).slice(1, -1).reduce((acc, crr) => acc + crr, 0) : 0;
 
-// test: should return 'A'
-console.log(getGrade(95,90,93));
+// test: should return -28
+console.log(sumArray([ -6, -20, -1, -10, -12 ]));
