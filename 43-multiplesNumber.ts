d1eacb7aac9f4671ded2d52a88bddc5e0596ec21
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-02-29 03:55:55 +0700

    add new kata

---
+++ 43-multiplesNumber.ts
@@ -0,0 +1,21 @@
+// Coding Everyday: Find Multiples of a Number
+// February 29, 2024
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd
+
+// Solution #01
+// export function findMultiples(integer: number, limit: number): number[] {
+//   const numbers: number[] = [];
+//   for (let i = integer; i <= limit; i += integer) {
+//     numbers.push(i);
+//   }
+//   return numbers;
+// }
+
+// Solution #02 by Brettm12345
+export const findMultiples = (int: number, limit: number): number[] =>
+  [...Array(limit).keys()]
+    .map(value => value + 1)
+    .filter(value => value % int === 0)
+
+// test: should return [5, 10, 15, 20, 25]
+console.log(findMultiples(5, 25))
