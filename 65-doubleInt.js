98cc7aa15463fc5e996e1b2958ff24983b5b2014
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-03-23 16:48:54 +0700

    add new kata

---
+++ 65-doubleInt.js
@@ -0,0 +1,14 @@
+// Coding Everyday: You Can't Code Under Pressure #1
+// March 23, 2024
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// Solution #01
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// Solution #02 
+const doubleInteger = (i) => i * 2;
+
+// test: should return [1, 2, 2, 3]
+console.log(flip('R', [3, 2, 1, 2]))
