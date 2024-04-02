1b356262c486e02f3da71fa437b60dc08259689f
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-04-03 02:24:27 +0700

    add new kata

---
+++ 75-cuboidVol.js
@@ -0,0 +1,18 @@
+// Coding Everyday: Volume of a Cuboid
+// April 03, 2024
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// Solution #01
+// class Kata {
+//   static getVolumeOfCuboid(length, width, height) {
+//     return length * width * height;
+//   }
+// }
+
+// Solution #02 by jonathanobino, Balkoth, kamodev, WiebeCnossen, Daniel Efremov, IvandCodes, vladpro3, user3499539, talbi belgacem, wajdi13 (+ 77)
+const Kata = {
+  getVolumeOfCuboid : (length, width, height) => length * width * height
+}
+
+// test: should return 4
+console.log(getVolumeOfCuboid(1,2,2))
