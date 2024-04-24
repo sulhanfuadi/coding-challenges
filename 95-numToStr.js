49ac736864fa46187182e6c1fcb3bbfb322199a7
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-04-24 07:02:45 +0700

    add new kata

---
+++ 95-numToStr.js
@@ -0,0 +1,14 @@
+// Convert a Number to a String!
+// April 24, 2024
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+// Solution #01
+function numberToString(num) {
+  return num.toString();
+}
+
+// Solution #02 by Shraymonks, freakyscream, dorayme, 0xjgv, thogrego, wgrisa, alevet, PanMaciek, sztadii, Leon Knabel (+ 480)
+const numberToString = num => `${num}`;
+
+// test: should return '67'
+console.log(numberToString(67))
