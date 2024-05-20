117710e9854fb9aae133ffa652824b8ed3a80f25
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-05-20 07:57:17 +0700

    add new kata

--- 120-kata.js
+++ 120-kata.js
@@ -3,10 +3,16 @@
 // https://www.codewars.com/kata/55225023e1be1ec8bc000390
 
 // Solution #01
+// function greet(name){
+//   if(name === "Johnny")
+//     return "Hello, my love!";
+//   return "Hello, " + name + "!";
+// }
 
+// Solution #02 by colbydauph, ostaladaFab, oussouFal, Rayen963
+let greet = name => `Hello, ${name == "Johnny" ? "my love" : name}!`;
 
-// Solution #02 by vatson
+// test:
+console.log(greet("Jim"));
 
-
-// test: should return 'Publish!'
-console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
+console.log(greet("Johnny"));
---
+++ 121-kata.js
@@ -0,0 +1,12 @@
+// Configure package json for a node application
+// May 21, 2024
+// https://www.codewars.com/kata/5692a582cc22387ccf000017
+
+// Solution #01
+
+
+// Solution #02 by vatson
+
+
+// test: should return 'Publish!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
