807cdd953e53affc04647a5742c0d9127d2936fb
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-05-29 06:14:53 +0700

    add new kata

---
+++ 129-Cube.js
@@ -0,0 +1,22 @@
+// Playing with cubes II
+// May 29, 2024
+// https://www.codewars.com/kata/55c0ac142326fdf18d0000af
+
+// Solution #01
+// This Cube class needs help
+// Implement the constructor so that it can take an integer for the side or no args
+class Cube {
+  
+    getSide() {
+      return this.side; 
+    }
+    setSide(n) {
+      this.side = Math.abs(n);
+    }
+  }
+
+// Solution #02 by vatson
+
+
+// test: should return 'xxxxxx'
+console.log(howMuchWater(10,10,21));
