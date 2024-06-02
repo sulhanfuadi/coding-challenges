d038e944869466288936152bb9e548c65de17ec7
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-06-02 10:55:42 +0700

    add new kata

--- 131-digit.js
+++ 131-digit.js
@@ -4,7 +4,13 @@
 
 // Solution #01
 String.prototype.digit = function() {
-  return false;
+  if (this.length !== 1) {
+    return false;
+  }
+  return '0123456789'.includes(this);
 };
 
-// Solution #02 by vatson
+// Solution #02 by brunolm, Balkoth, Shraymonks, ooflorent, ysurilov, pwynn, slicklash, timp, user3780052, gevg (+ 1162)
+String.prototype.digit = function() {
+  return /^\d$/.test(this);
+};
