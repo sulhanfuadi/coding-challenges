50f20f7b8fed8f1f3d16b23bdfc8c710ce7fc81f
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-06-03 00:03:59 +0700

    add new kata

---
+++ 132-closeCompare.js
@@ -0,0 +1,14 @@
+// Compare within margin
+// June 02, 2024
+// https://www.codewars.com/kata/56453a12fcee9a6c4700009c
+
+// Solution #01
+function closeCompare(a, b, margin){
+  // ...
+}
+
+// Solution #02 by vatson
+
+
+// test: should return -1
+console.log(closeCompare(4, 5));
---
+++ 133-warnTheSheep.js
@@ -0,0 +1,11 @@
+// A wolf in sheep's clothing
+// June 03, 2024
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
+
+// Solution #01
+function warnTheSheep(queue) {
+
+}
+
+// Solution #02 by vatson
+
