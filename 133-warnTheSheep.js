99b5f9aa109f07da4619afc289dc4805bcf56a72
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-06-05 20:31:43 +0700

    add new kata

--- 133-warnTheSheep.js
+++ 133-warnTheSheep.js
@@ -1,10 +1,13 @@
 // A wolf in sheep's clothing
-// June 03, 2024
+// June 05, 2024
 // https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15
 
 // Solution #01
 function warnTheSheep(queue) {
-
+    const reversedQueue = queue.reverse();
+    const wolfPosition = reversedQueue.indexOf('wolf');
+    if (wolfPosition === 0) return 'Pls go away and stop eating my sheep';
+    return `Oi! Sheep number ${wolfPosition}! You are about to be eaten by a wolf!`;
 }
 
 // Solution #02 by vatson
