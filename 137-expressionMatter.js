c46893b6508aa9d4a6b14951e54b7fb7b53abadd
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-06-09 18:35:40 +0700

    add new kata

---
+++ 137-expressionMatter.js
@@ -0,0 +1,14 @@
+// Expressions Matter
+// June 09, 2024
+// https://www.codewars.com/kata/575fa9afee048b293e000287
+
+// Solution #01
+function expressionMatter(a, b, c) {
+  return // highest achievable result
+}
+
+// Solution #02 by vatson
+
+
+// test: should return 6
+console.log(expressionMatter(2, 1, 2));
