ebbeb0ff176e7281bff271f540acb1c0585714c4
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-06-17 06:07:16 +0700

    add new kata

--- 141-binToDec.js
+++ 141-binToDec.js
@@ -3,12 +3,12 @@
 // https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
 
 // Solution #01
-function binToDec(bin) {
-  // TODO
-}
+// function binToDec(bin) {
+// 	return parseInt(bin, 2);
+// }
 
-// Solution #02 by codeMan
-
+// Solution #02 by hencethus
+const binToDec = bin => [...bin].reduce((dec, bit) => dec << 1 | bit, 0);
 
 // test: should return 73
 console.log(binToDec('1001001'));
