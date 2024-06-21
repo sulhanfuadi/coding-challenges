5676e560b3758066c8a827d712e665d7d10da988
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-06-21 09:16:56 +0700

    add new kata

---
+++ 145-plural.js
@@ -0,0 +1,14 @@
+// Plural
+// June 21, 2024
+// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a
+
+// Solution #01
+function plural(n) {
+  return n === 1 ? false : true;
+}
+
+// Solution #02 by shadowmanos, Shraymonks, kevinlebrun, crimbus, nmanolov, taras.shtyfurko, tbrisbout, ppgnutzmann, insaslb, dschnelldavis (+ 404)
+// const plural = n => n !== 1;
+
+// test: should return false
+console.log(plural(1));
