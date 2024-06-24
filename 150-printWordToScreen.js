a085711322f1a51679796ffecbdb74a4786c14a0
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-06-24 10:20:52 +0700

    add new kata

---
+++ 150-printWordToScreen.js
@@ -0,0 +1,15 @@
+// Create your first JS function and print "Hello World!"
+// June 25, 2024
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
+
+// Solution #01
+const helloWorld = () => {
+    const str = 'Hello World!';
+    console.log(str);
+}
+
+// Solution #02 by vatson
+
+
+// test: should return 'Hello World!'
+console.log(helloWorld());
