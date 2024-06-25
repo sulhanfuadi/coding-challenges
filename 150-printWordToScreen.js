8757ba244eb4a0747213347fdaef5cb722c28cf3
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-06-25 10:13:28 +0700

    add new kata

--- 150-printWordToScreen.js
+++ 150-printWordToScreen.js
@@ -3,13 +3,20 @@
 // https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8
 
 // Solution #01
-const helloWorld = () => {
-    const str = 'Hello World!';
-    console.log(str);
+// const helloWorld = () => {
+//     const str = 'Hello World!';
+//     console.log(str);
+// }
+
+// Solution #02 by user3220651
+function helloWorld(){
+    var regex = new RegExp(',', 'g');
+    var str = null;
+    var poop = ["H","e","l","l","o"," ","W","o","r","l","d","!"];
+    poop = poop.toString();
+    poop = poop.replace(regex, '');
+    console.log(poop);
 }
-
-// Solution #02 by vatson
-
 
 // test: should return 'Hello World!'
 console.log(helloWorld());
---
+++ 151-stringClean.js
@@ -0,0 +1,14 @@
+// String cleaning
+// June 25, 2024
+// https://www.codewars.com/kata/57e1e61ba396b3727c000251
+
+// Solution #01
+function stringClean(s){
+  // Function will return the cleaned string
+}
+
+// Solution #02 by vatson
+
+
+// test: should return "(Eat me!!)"
+console.log(stringClean("(E3at m2e2!!)"));
