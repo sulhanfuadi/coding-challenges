a5db68aff1866fb832ee40c860ac4d1af2597d37
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-06-26 08:08:35 +0700

    add new kata

--- 151-stringClean.js
+++ 151-stringClean.js
@@ -1,14 +1,22 @@
 // String cleaning
-// June 25, 2024
+// June 26, 2024
 // https://www.codewars.com/kata/57e1e61ba396b3727c000251
 
 // Solution #01
 function stringClean(s){
-  // Function will return the cleaned string
+  let result = '';
+  for (i = 0; i < s.length; i++) {
+    if (isNaN(s[i]) || s[i] === ' ') {
+      result += s[i];
+    }
+  }
+  return result;
 }
 
-// Solution #02 by vatson
-
+// Solution #02 by Knochenmark, Woltemade, AgathaLynn, ThomasConto, fdiogo, Michael Hansen, jkm92116, ScratchFandango, K-AlfredIwasaki, LydiaLyd (+ 1746)
+function stringClean(s){
+  return s.replace(/\d/g, "");
+}
 
 // test: should return "(Eat me!!)"
 console.log(stringClean("(E3at m2e2!!)"));
