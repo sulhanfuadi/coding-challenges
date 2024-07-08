e589631e67b6ac40373880cadaa3db03a2855c64
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-07-08 11:46:23 +0700

    add new kata

---
+++ 159-wrap.js
@@ -0,0 +1,13 @@
+// Semi-Optional
+// July 08, 2024
+// https://www.codewars.com/kata/521cd52e790405a74800032c
+
+// Solution #01
+function wrap(value) {
+  return {
+    value: value,
+  };
+}
+
+// Solution #02 by Shraymonks, htar, !ming, lndgalante, nahilep, s21n, RslnMtvv, ben-eb, user5848499, 2PR (+ 151)
+const wrap = (value) => ({ value });
