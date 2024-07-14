4a27acff8ba0734f1049265ca26cc8ab3b0b68bf
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-07-14 11:11:36 +0700

    add new kata

--- 165-testEven.js
+++ 165-testEven.js
@@ -1,13 +1,17 @@
 // Is it even?
 // July 14, 2024
-// https://www.codewars.com/kata/xxxxxxxxxxxxxxxxxxxx
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5
 
 // Solution #01
+// function testEven(n) {
+//   return n % 2 === 0;
+// }
+
+// Solution #02 by dmitriisurmin
 function testEven(n) {
-  //Your awesome code here!
+  /// ( ¬‿¬) ///
+  return !(+n.toString(2).charAt(n.toString(2).length - 1) & 1);
 }
 
-// Solution #02 by ysurilov
-
-// test: should return 'xxxxxx'
-console.log(howMuchWater(10, 10, 21));
+// test: should return true
+console.log(testEven(-4));
