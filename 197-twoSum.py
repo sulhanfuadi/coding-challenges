e5f8032e003ae6613dd6d030121d92bd8b82f35c
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-08-22 09:31:17 +0700

    add new problem

---
+++ 197-twoSum.py
@@ -0,0 +1,9 @@
+# Lagipula Hidup Akan Berakhir
+# August 22, 2024
+# https://leetcode.com/problems/two-sum/
+
+# Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+
