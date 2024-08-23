3f6d0f7d77a574e6657beff7f9c9338f892bb9d8
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-08-23 11:28:12 +0700

    add a soultion

--- 197-twoSum.py
+++ 197-twoSum.py
@@ -1,9 +1,16 @@
-# Lagipula Hidup Akan Berakhir
+# 1. Two Sum
 # August 22, 2024
 # https://leetcode.com/problems/two-sum/
 
 # Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        prevMap = {} # val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return None
 
