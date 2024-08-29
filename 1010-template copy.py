37a886c59ce403d473f834ee49612d7e173ff835
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-08-29 10:34:18 +0700

    add new problem

--- 1010-template copy.py
+++
@@ -1,6 +0,0 @@
-# Lagipula Hidup Akan Berakhir
-# August 21, 2024
-# https://leetcode.com/problems/xxxxxxxxxxxxxxxxxxxx
-
-# Solution
-
---
+++ 200-rob.py
@@ -0,0 +1,8 @@
+# 198. House Robber
+# August 29, 2024
+# https://leetcode.com/problems/house-robber
+
+# Solution
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
