18f0281b5e81ff04b01117d33480f179e2ac323a
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-08-30 10:18:03 +0700

    add new code

--- 200-rob.py
+++ 200-rob.py
@@ -5,4 +5,12 @@
 # Solution
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        rob1, rob2 = 0, 0
+        # [rob1, rob2, n, n+1, ...]
+        for n in nums:
+            temp = max(n + rob1, rob2) # meaning: if we rob this house, we can't rob the previous house
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+    # Time complexity: O(n)
+    # Space complexity: O(1)
