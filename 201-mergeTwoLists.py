094fdb43c678ac3e0d9e7070b5b6494495003f4f
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-03 09:30:13 +0700

    add new code

--- 201-mergeTwoLists.py
+++ 201-mergeTwoLists.py
@@ -9,23 +9,40 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # ListNode() is a class that creates a node with val and next attributes
-        dummy = ListNode()
-        tail = dummy
+    # def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    #     # ListNode() is a class that creates a node with val and next attributes
+    #     dummy = ListNode()
+    #     tail = dummy
 
+    #     while list1 and list2:
+    #         if list1.val < list2.val:
+    #             # tail.next is the next node of the tail
+    #             tail.next = list1
+    #             l1 = list1.next
+    #         else:
+    #             tail.next = list2
+    #             l2 = list2.next
+        
+    #     if list1:
+    #         tail.next = list1
+    #     elif list2:
+    #         tail.next = list2
+        
+    #     return dummy.next
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr = ListNode()
+        
         while list1 and list2:
             if list1.val < list2.val:
-                # tail.next is the next node of the tail
-                tail.next = list1
-                l1 = list1.next
+                curr.next = list1
+                list1 = list1.next
             else:
-                tail.next = list2
-                l2 = list2.next
-        
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-        
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+                    
+        curr.next = list1 or list2
+                    
         return dummy.next
+
