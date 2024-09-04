e028c59cd9d8011dd44bdd097cc9e0b1844f0a7b
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-04 18:56:59 +0700

    add new problem

---
+++ 202-maxProfit.py
@@ -0,0 +1,8 @@
+# 121. Best Time to Buy and Sell Stock
+# September 04, 2024
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+# Solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
