17f7559d73f6bb5d2e1340726bef2dbea8b40981
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-05 10:30:11 +0700

    add new code

--- 202-maxProfit.py
+++ 202-maxProfit.py
@@ -5,4 +5,15 @@
 # Solution
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        l, r = 0, 1 # left and right pointers, left = buy, right = sell
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
+
+        return max_profit
