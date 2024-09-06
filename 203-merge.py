db88fbea61a30a6d382bbf0ab069326fabb1c59f
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-06 09:48:33 +0700

    add new problem

---
+++ 203-merge.py
@@ -0,0 +1,11 @@
+# 88. Merge Sorted Array
+# September 06, 2024
+# https://leetcode.com/problems/merge-sorted-array
+
+# Solution
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
