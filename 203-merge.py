6d995a41c163bc45f83c7be85003503e3512397f
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-08 11:18:19 +0700

    add new code

--- 203-merge.py
+++ 203-merge.py
@@ -5,7 +5,22 @@
 # Solution
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+        # last index of nums1
+        last = m + n - 1
+
+        # merge in reverse order
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]: # index starts from 0, so its n - 1, not just n
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1
+        
+        # if there are still elements in nums2
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+            last -= 1 
         
