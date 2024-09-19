7af0d7fd94f64f46ca8bce5e154a5b4fc3e4d02b
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-19 09:51:45 +0700

    add new solution

--- 207-isValid.py
+++ 207-isValid.py
@@ -5,4 +5,15 @@
 # Solution
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        stack = []
+        closeToOpen = {')': '(', '}': '{', ']': '['}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
