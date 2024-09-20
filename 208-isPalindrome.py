c7bfc5a56bf57f2a817e489fb90b210d28fcf5c3
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-20 07:07:12 +0700

    add new problem

---
+++ 208-isPalindrome.py
@@ -0,0 +1,13 @@
+# Lagipula Hidup Akan Berakhir
+# September 20, 2024
+# https://leetcode.com/problems/xxxxxxxxxxxxxxxxxxxx
+
+# Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
