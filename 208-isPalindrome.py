7b082db629cefe9982d93c52bceca94a98f21ef5
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-21 22:10:23 +0700

    add new code

--- 208-isPalindrome.py
+++ 208-isPalindrome.py
@@ -1,6 +1,6 @@
-# Lagipula Hidup Akan Berakhir
+# 234. Palindrome Linked List
 # September 20, 2024
-# https://leetcode.com/problems/xxxxxxxxxxxxxxxxxxxx
+# https://leetcode.com/problems/palindrome-linked-list
 
 # Solution
 # Definition for singly-linked list.
@@ -9,5 +9,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+    def isPalindromeV01(self, head: Optional[ListNode]) -> bool:
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+        # Initialize two pointers, one at the start and one at the end of the list
+        l, r = 0, len(nums) - 1
+        # Check if the list is a palindrome by comparing values from both ends
+        while l < r:
+            if nums[l] != nums[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+    
+    def isPalindromeV02(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+
+        # Find the middle of the list (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # Reverse the second half of the list
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
         
+        # Compare the first half and the reversed second half
+        left, right = head, prev
+        while prev:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        return True
