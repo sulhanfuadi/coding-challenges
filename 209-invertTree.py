3dd6b0abf201ee43a88c66bcba6101395e689498
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-23 08:48:33 +0700

    add new code

--- 209-invertTree.py
+++ 209-invertTree.py
@@ -11,4 +11,15 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
         
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
