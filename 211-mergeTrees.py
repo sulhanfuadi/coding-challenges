d7ab8bce3a0a8eb71d5c659c5d22ac6e8fd4729f
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-09-27 07:23:26 +0700

    add new code

--- 211-mergeTrees.py
+++ 211-mergeTrees.py
@@ -4,11 +4,21 @@
 
 # Solution
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1 and not t2:
+            return None
+        v1 = t1.val if t1 else 0
+        v2 = t2.val if t2 else 0
+        root = TreeNode(v1+v2)
+
+        root.left = self.mergeTrees(t1 and t1.left, t2 and t2.left)
+        root.right = self.mergeTrees(t1 and t1.right, t2 and t2.right)
+
+        return root
