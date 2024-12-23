1c4b96f8774c40599ea741a2d82d63ff103f1918
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-12-23 10:20:38 +0700

    add new code

--- 213-lowestCommonAncestor.py
+++ 213-lowestCommonAncestor.py
@@ -1,5 +1,5 @@
 # 235. Lowest Common Ancestor of a Binary Search Tree
-# December 22, 2024
+# December 23, 2024
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
 
 # Solution
@@ -12,4 +12,12 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+        cur = root
+
+        while cur:
+            if p.val > cur.val and q.val > cur.val: # if both p and q are greater than cur, then move to the right
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val: # if both p and q are less than cur, then move to the left
+                cur = cur.left
+            else: # if p and q are on different sides of cur, then cur is the LCA
+                return cur
