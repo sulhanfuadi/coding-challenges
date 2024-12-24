c9b4083c0660cc5163b7453daf72c8ebdba1fbf4
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-12-24 16:06:36 +0700

    add new code

--- 214-happyNumber.py
+++ 214-happyNumber.py
@@ -5,4 +5,20 @@
 # Solution
 class Solution:
   def isHappy(self, n: int) -> bool:
-    
+    visit = set() # store the visited numbers # MEMORY O(N)
+
+    while n not in visit: # TIME O(N) where N is the number of digits in n
+      visit.add(n)
+      n = self.sumOfSquares(n) # sum of the squares of the digits of n
+
+      if n == 1: # if n is 1, then n is a happy number
+        return True
+      
+    return False
+  
+  def sumOfSquares(self, n: int) -> int:
+    sum = 0
+    while n:
+      sum += (n % 10) ** 2 # add the square of the last digit of n
+      n //= 10 # remove the last digit of n
+    return sum
