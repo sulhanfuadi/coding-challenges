bfa0042d3b991a2a336858d47bc598416ab541a2
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-12-27 12:46:23 +0700

    add new problem

---
+++ 217-searchInsert.py
@@ -0,0 +1,8 @@
+# 35. Search Insert Position
+# December 27, 2024
+# https://leetcode.com/problems/search-insert-position
+
+# Solution
+class Solution:
+  def searchInsert(self, nums: List[int], target: int) -> int:
+    
