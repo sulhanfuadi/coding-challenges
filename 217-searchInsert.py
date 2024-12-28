0db2603602b72eea54a8c527894b002576db16f9
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-12-28 11:10:17 +0700

    feat: Add search insert position solution
    Added a binary search solution for the 'Search Insert Position' problem from LeetCode. The function takes a sorted array and a target value, and returns the index where the target should be inserted to maintain the sorted order.

--- 217-searchInsert.py
+++ 217-searchInsert.py
@@ -5,4 +5,17 @@
 # Solution
 class Solution:
   def searchInsert(self, nums: List[int], target: int) -> int:
-    
+    l, r = 0, len(nums) - 1 # left and right pointers
+
+    while l <= r: # while left pointer is less than or equal to right pointer
+      mid = (l + r) // 2 # middle index
+
+      if target == nums[mid]: # if target is found, return the index
+        return mid
+
+      if target > nums[mid]: # if target is greater than the middle element, move left pointer to mid + 1
+        l = mid + 1
+      else: # if target is less than the middle element, move right pointer to mid - 1
+        r = mid - 1
+
+    return l # return the left pointer if target is not found
