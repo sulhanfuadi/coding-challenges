8bf7f2417a175096d89389891d1d6104217e3c9a
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-12-29 08:35:36 +0700

    add new problem

---
+++ 218-lastStoneWeight.py
@@ -0,0 +1,8 @@
+# 1046. Last Stone Weight
+# December 30, 2024
+# https://leetcode.com/problems/last-stone-weight
+
+# Solution
+class Solution:
+  def lastStoneWeight(self, stones: List[int]) -> int:
+    
