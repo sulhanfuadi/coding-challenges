5a7efc97548a905577b41ffbd3d2e50d99843c4b
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-12-30 18:15:54 +0700

    feat: Implement last stone weight solution
    - Added a solution for the Last Stone Weight problem using a max heap approach.
    - Converted stone weights to negative values to simulate a max heap using Python's min heap.
    - Implemented logic to repeatedly smash the two heaviest stones until one or none remains.
    - Returned the weight of the last remaining stone or 0 if no stones remain.

--- 218-lastStoneWeight.py
+++ 218-lastStoneWeight.py
@@ -3,6 +3,19 @@
 # https://leetcode.com/problems/last-stone-weight
 
 # Solution
+import heapq
+from typing import List
+
 class Solution:
   def lastStoneWeight(self, stones: List[int]) -> int:
+    stones = [-stone for stone in stones] # convert to negative values`
+    heapq.heapify(stones) # convert to heap
     
+    while len(stones) > 1: # while there are more than 1 stones
+      first = heapq.heappop(stones) # get the first stone, the heaviest
+      second = heapq.heappop(stones) # get the second stone, the second heaviest
+
+      if first != second: # if the stones are not equal
+        heapq.heappush(stones, first - second) # push the difference back to the heap
+
+    return abs(stones[0]) if stones else 0 # return the last stone if there is one, else return 0
