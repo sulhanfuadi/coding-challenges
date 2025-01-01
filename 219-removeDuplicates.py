e5c967f4ac734755f801f9f17d00bd73b88e8c85
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-01 09:44:33 +0700

    feat: add solution for removing duplicates from sorted array
    Added a new solution in 219-removeDuplicates.py to remove duplicates from a sorted array. The solution iterates over the array and updates it with unique values, returning the length of the new array.

--- 219-removeDuplicates.py
+++ 219-removeDuplicates.py
@@ -5,4 +5,11 @@
 # Solution
 class Solution:
   def removeDuplicates(self, nums: List[int]) -> int:
+    l = 1 # pointer to the last element of the new array
+
+    for r in range(1, len(nums)): # iterate over the array
+      if nums[r] != nums[r - 1]: # if the current element is different from the previous one (new unique value)
+        nums[l] = nums[r] # update the new array with the new unique value
+        l += 1 # increment the left pointer
     
+    return l # return the length of the new array, which is the left pointer
