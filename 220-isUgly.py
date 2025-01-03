2f0051857d151c519050d8f457ea53ffe474266f
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-03 10:59:51 +0700

    feat: add solution for checking if a number is ugly
    Added a solution for the LeetCode problem '263. Ugly Number'. The solution includes a method 'isUgly' which checks if a given number is an ugly number by dividing it by 2, 3, and 5 until it is no longer divisible by these primes. If the resulting number is 1, it returns True; otherwise, it returns False.

--- 220-isUgly.py
+++ 220-isUgly.py
@@ -5,4 +5,11 @@
 # Solution
 class Solution:
   def isUgly(self, n: int) -> bool:
+    if n <= 0: # if n is less than or equal to 0, return False
+      return False
     
+    for p in [2, 3, 5]: # iterate through the prime numbers
+      while n % p == 0: # while n is divisible by p
+        n //= p # divide n by p
+    
+    return n == 1 # return True if n is equal to 1, else False
