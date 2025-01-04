485d8dd85e63eb2ef325c27b0e22fd32865a52ba
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-04 09:50:07 +0700

    add new problem

---
+++ 221-lengthOfLastWord.py
@@ -0,0 +1,8 @@
+# 58. Length of Last Word
+# January 05, 2025
+# https://leetcode.com/problems/length-of-last-word/
+
+# Solution
+class Solution:
+  def lengthOfLastWord(self, s: str) -> int:
+    
