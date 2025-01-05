eec51b20ac42153f1ef9d36955bd266c3aaa9c25
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-05 15:14:59 +0700

    feat: add solution for length of last word problem
    Implemented a solution for the 'Length of Last Word' problem from LeetCode. The solution includes a method to calculate the length of the last word in a given string by iterating from the end of the string, skipping trailing spaces, and counting the characters of the last word.

--- 221-lengthOfLastWord.py
+++ 221-lengthOfLastWord.py
@@ -5,4 +5,16 @@
 # Solution
 class Solution:
   def lengthOfLastWord(self, s: str) -> int:
+    i = len(s) - 1  # start from the end of the string
+    length = 0   # initialize the length of the last word
+
+    # skip the trailing spaces
+    while i >= 0 and s[i] == ' ': # if the character is a space
+      i -= 1
+
+    # count the length of the last word
+    while i >= 0 and s[i] != ' ':
+      length += 1
+      i -= 1
     
+    return length
