d2937a73c7f9732e7cd4218c4c05aa88809b9de4
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-06 09:14:11 +0700

    feat: add initial setup for remove element problem
    Added initial setup for the 'Remove Element' problem from LeetCode. The setup includes the class definition and the method signature for the solution.

---
+++ 222-removeElement.py
@@ -0,0 +1,8 @@
+# 27. Remove Element
+# January 07, 2025
+# https://leetcode.com/problems/remove-element/
+
+# Solution
+class Solution:
+  def removeElement(self, nums: List[int], val: int) -> int:
+    
