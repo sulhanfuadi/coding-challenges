476e20960b96ed35aa1072ef7e083953c41c6f14
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-08 11:58:29 +0700

    feat: add initial setup for unique email addresses problem
    Added initial setup for the 'Unique Email Addresses' problem from LeetCode. The setup includes the class definition and the method signature for the solution.

---
+++ 223-numUniqueEmails.py
@@ -0,0 +1,8 @@
+# 929. Unique Email Addresses
+# January 09, 2025
+# https://leetcode.com/problems/unique-email-addresses/
+
+# Solution
+class Solution:
+  def numUniqueEmails(self, emails: List[str]) -> int:
+    
