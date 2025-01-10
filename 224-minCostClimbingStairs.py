bfd32e97783b392b0c7dfce67af0fddf16f20d76
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-10 07:42:30 +0700

    feat: add initial setup for min cost climbing stairs problem
    Added initial setup for the 'Min Cost Climbing Stairs' problem from LeetCode. The setup includes the class definition and the method signature for the solution.

---
+++ 224-minCostClimbingStairs.py
@@ -0,0 +1,8 @@
+# 746. Min Cost Climbing Stairs
+# January 11, 2025
+# https://leetcode.com/problems/min-cost-climbing-stairs
+
+# Solution
+class Solution:
+  def minCostClimbingStairs(self, cost: List[int]) -> int:
+    
