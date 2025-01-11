7bfa8816dfcc6c4ada4ba3800c7b13ebf334a5e1
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-11 12:32:25 +0700

    feat: implement solution for min cost climbing stairs problem
    Completed the implementation for the 'Min Cost Climbing Stairs' problem from LeetCode. The solution calculates the minimum cost to reach the top of the stairs by iterating from the end of the list and updating each element with the minimum cost of the next two steps.

--- 224-minCostClimbingStairs.py
+++ 224-minCostClimbingStairs.py
@@ -5,4 +5,9 @@
 # Solution
 class Solution:
   def minCostClimbingStairs(self, cost: List[int]) -> int:
-    
+    cost.append(0) # add 0 to the end of the list, so that we can reach the top of the stairs
+
+    for i in range(len(cost) - 3, -1, -1): # start from the second to last element, decrementing by 1
+      cost[i] += min(cost[i + 1], cost[i + 2]) # add the minimum of the next two elements to the current element
+
+    return min(cost[0], cost[1]) # return the minimum of the first two elements
