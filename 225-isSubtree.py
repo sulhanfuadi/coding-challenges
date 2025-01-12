eb88ab6a9f556808ea6562c4a0f9664595b85c04
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-12 08:29:08 +0700

    feat: add initial setup for subtree of another tree problem
    Added initial setup for the 'Subtree of Another Tree' problem from LeetCode. The setup includes the class definition and the method signature for the solution.

---
+++ 225-isSubtree.py
@@ -0,0 +1,14 @@
+# 572. Subtree of Another Tree
+# January 13, 2025
+# https://leetcode.com/problems/subtree-of-another-tree/
+
+# Solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    
