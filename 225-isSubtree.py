ca69902e684bb6e4b1ded3df380037ee9e3fa582
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-13 11:07:47 +0700

    feat: implement solution for subtree of another tree problem
    Completed the implementation for the 'Subtree of Another Tree' problem from LeetCode. The solution includes methods to check if one tree is a subtree of another by comparing nodes and their subtrees recursively.

--- 225-isSubtree.py
+++ 225-isSubtree.py
@@ -11,4 +11,19 @@
 #         self.right = right
 class Solution:
   def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    if not subRoot: # subRoot is None, None is a subtree of any tree
+      return True
+    if not root: # root is None, so there is no way that subRoot is a subtree of root
+      return False
+    if self.sameSubtree(root, subRoot): # if the trees are the same, it's mean subRoot is a subtree of root
+      return True
+    return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) # check left and right subtrees, recursively
     
+  def sameSubtree(self, tree1, tree2):
+    if not tree1 and not tree2: # both are None
+      return True
+    if not tree1 or not tree2: # one of them is None
+      return False
+    if tree1.val != tree2.val: # values are different
+      return False
+    return self.sameSubtree(tree1.left, tree2.left) and self.sameSubtree(tree1.right, tree2.right) # check left and right subtrees, recursively
