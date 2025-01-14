1cb89904cfb55d372908cc5006ab23088cf17cbe
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-15 06:22:09 +0700

    feat: add new solution for valid palindrome problem
    Added a new solution for the 'Valid Palindrome' problem from LeetCode. The new solution uses two pointers to compare characters from both ends, ignoring non-alphanumeric characters.

--- 226-isPalindrome.py
+++ 226-isPalindrome.py
@@ -13,3 +13,30 @@ class Solution:
 
     return newStr == newStr[::-1] # check if the new string is equal to the reverse of the new string
   
+  def isPalindrome2(self, s: str) -> bool:
+    l, r = 0, len(s) - 1 # left and right pointers
+
+    # loop through the string, until the left pointer is less than the right pointer, which means we have checked all the characters
+    while l < r:
+      # check if the character at the left pointer is not alphanumeric
+      while l < r and not self.alphaNumeric(s[l]):
+        l += 1
+      # check if the character at the right pointer is not alphanumeric
+      while l < r and not self.alphaNumeric(s[r]):
+        r -= 1
+
+      # check if the characters at the left and right pointers are not equal
+      if s[l].lower() != s[r].lower():
+        return False
+
+      # move the pointers
+      l += 1
+      r -= 1
+
+    return True # return True if the string is a palindrome
+
+  # helper function to check if the character is alphanumeric
+  def alphaNumeric(self, c: str) -> bool:
+    return ((ord('a') <= ord(c) <= ord('z')) or # check if the character is a letter, lowercase
+            (ord('A') <= ord(c) <= ord('Z')) or # check if the character is a letter, uppercase
+            (ord('0') <= ord(c) <= ord('9'))) # check if the character is a number
