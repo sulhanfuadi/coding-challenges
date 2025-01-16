7af651ee88160742ac9748164bce67fe764b94ca
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-16 12:55:29 +0700

    feat: add initial setup for isomorphic strings problem
    Added initial setup for the 'Isomorphic Strings' problem from LeetCode. The setup includes the class definition and the method signature for the solution.

---
+++ 227-isIsomorphic.py
@@ -0,0 +1,8 @@
+# 205. Isomorphic Strings
+# January 17, 2025
+# https://leetcode.com/problems/isomorphic-strings
+
+# Solution
+class Solution:
+  def isIsomorphic(self, s: str, t: str) -> bool:
+    
