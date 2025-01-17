bbc698eaec6e9fdd895fe4ff457013a0d2ce9261
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-17 13:38:15 +0700

    feat: implement solution for isomorphic strings problem
    Completed the implementation for the 'Isomorphic Strings' problem from LeetCode. The solution uses two dictionaries to map characters from one string to the other and checks for consistency in the mappings.

--- 227-isIsomorphic.py
+++ 227-isIsomorphic.py
@@ -5,4 +5,18 @@
 # Solution
 class Solution:
   def isIsomorphic(self, s: str, t: str) -> bool:
-    
+    mapST, mapTS = {}, {} # map from s to t and t to s
+
+    for i in range(len(s)):
+      c1, c2 = s[i], t[i] # get the characters at the same index
+
+      if c1 in mapST and mapST[c1] != c2: # if c1 is already mapped to another character
+        return False
+
+      if c2 in mapTS and mapTS[c2] != c1: # if c2 is already mapped to another character
+        return False
+
+      mapST[c1] = c2 # map s to t, if it already exists, it should be the same
+      mapTS[c2] = c1 # map t to s, if it already exists, it should be the same
+
+    return True # if no mismatches are found, return True
