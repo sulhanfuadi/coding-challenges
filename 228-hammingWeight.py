445fab7d6ac1d2fffc19dcd36120aa8eae79d193
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-20 10:39:50 +0700

    feat: add new solutions for number of 1 bits problem
    Added two new solutions for the 'Number of 1 Bits' problem from LeetCode. The second solution uses bit manipulation to remove the rightmost 1 bit and count the number of 1s. The third solution converts the integer to binary and counts the number of 1s.

--- 228-hammingWeight.py
+++ 228-hammingWeight.py
@@ -10,3 +10,13 @@ class Solution:
       result += n & 2 # for each bit, if it is 1, add 1 to result, if it is 0, add 0 to result
       n >>= 1 # shift n to the right by 1 bit
     return result
+
+  def hammingWeight2(self, n: int) -> int:
+    result = 0
+    while n:
+      n &= n - 1 # n & (n - 1) will remove the rightmost 1 in n and update n
+      result += 1 # count the number of 1s removed
+    return result
+
+  def hammingWeight3(self, n: int) -> int:
+    return bin(n).count('1') # convert n to binary and count the number of 1s
