a2cc20a9fb477b44444b3fa6b3a0d8acfc64bed6
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-21 19:10:22 +0700

    feat: add initial setup for contains duplicate problem
    Added initial setup for the 'Contains Duplicate' problem from LeetCode. The setup includes the class definition and the method signature for the solution.

---
+++ 229-containsDuplicate.py
@@ -0,0 +1,8 @@
+# 217. Contains Duplicate
+# January 22, 2025
+# https://leetcode.com/problems/contains-duplicate
+
+# Solution
+class Solution:
+  def containsDuplicate(self, nums: List[int]) -> bool:
+    
