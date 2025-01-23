4508e65741acf21f659b372218d67b3024e5bea3
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-23 20:02:24 +0700

    feat: add initial setup for Kth largest element in a stream problem
    Added initial setup for the 'Kth Largest Element in a Stream' problem from LeetCode. The setup includes the class definition and method signatures for the solution.

---
+++ 230-KthLargest.py
@@ -0,0 +1,11 @@
+# 703. Kth Largest Element in a Stream
+# January 24, 2025
+# https://leetcode.com/problems/kth-largest-element-in-a-stream
+
+# Solution
+class KthLargest:
+  def __init__(self, k: int, nums: List[int]):
+    
+
+  def add(self, val: int) -> int:
+    
