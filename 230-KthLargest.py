3c9720c528ec27c8cae870b098848e01af2b2018
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-24 13:20:47 +0700

    feat: implement solution for Kth largest element in a stream problem
    Completed the implementation for the 'Kth Largest Element in a Stream' problem from LeetCode. The solution uses a min-heap to maintain the k largest elements and provides a method to add new elements and retrieve the kth largest element.

--- 230-KthLargest.py
+++ 230-KthLargest.py
@@ -2,10 +2,26 @@
 # January 24, 2025
 # https://leetcode.com/problems/kth-largest-element-in-a-stream
 
+import heapq
+
 # Solution
 class KthLargest:
   def __init__(self, k: int, nums: List[int]):
-    
+    # minHeap with K largest elements
+    self.minHeap = nums # list of k largest elements
+    self.k = k # kth largest element
+
+    # heapify the list
+    heapq.heapify(self.minHeap)
+
+    # if the list has more than k elements, remove the smallest element
+    while len(self.minHeap) > k:
+      heapq.heappop(self.minHeap)
 
   def add(self, val: int) -> int:
-    
+    heapq.heappush(self.minHeap, val) # add the new number to the minHeap
+
+    if len(self.minHeap) > self.k: # if the heap has more than k elements, remove the smallest number
+      heapq.heappop(self.minHeap)
+
+    return self.minHeap[0] # return the Kth largest number
