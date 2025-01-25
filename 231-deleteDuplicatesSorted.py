90b982102b7b5aba0ed68c50fb024b9ce2668aa5
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-25 11:45:09 +0700

    feat: add initial setup for remove duplicates from sorted list problem
    Added initial setup for the 'Remove Duplicates from Sorted List' problem from LeetCode. The setup includes the class definition and method signature for the solution.

---
+++ 231-deleteDuplicatesSorted.py
@@ -0,0 +1,13 @@
+# 83. Remove Duplicates from Sorted List
+# January 26, 2025
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list
+
+# Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+  def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    
