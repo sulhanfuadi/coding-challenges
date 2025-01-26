dfada70b38f9e2390cfdc2a7cfa6a872dfacacf4
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-26 08:01:00 +0700

    feat: implement solution for remove duplicates from sorted list problem
    Completed the implementation for the 'Remove Duplicates from Sorted List' problem from LeetCode. The solution traverses the linked list and removes duplicate nodes by adjusting the next pointers.

--- 231-deleteDuplicatesSorted.py
+++ 231-deleteDuplicatesSorted.py
@@ -10,4 +10,13 @@
 #         self.next = next
 class Solution:
   def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    current = head # start at the head, the first element
+
+    # traverse the list
+    while current is not None and current.next is not None:
+      if current.val == current.next.val: # if the current value is the same as the next value
+        current.next = current.next.next
+      else:
+        current = current.next
     
+    return head
