8fba4bc205d0a299572d4e94bea1f0234379b6c2
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-27 11:38:15 +0700

    feat: add initial setup for can place flowers problem
    Added initial setup for the 'Can Place Flowers' problem from LeetCode. The setup includes the class definition and method signature for the solution.

---
+++ 232-canPlaceFlowers.py
@@ -0,0 +1,8 @@
+# 605. Can Place Flowers
+# January 28, 2025
+# https://leetcode.com/problems/can-place-flowers
+
+# Solution
+class Solution:
+  def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    
