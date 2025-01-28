8365bf3db16f17d14d2f8ef0436db3c1fe2d2cff
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-28 09:45:33 +0700

    feat: implement solution for can place flowers problem
    Completed the implementation for the 'Can Place Flowers' problem from LeetCode. The solution iterates through the flowerbed, checking if a flower can be planted in each position, and updates the flowerbed and the count of flowers to plant accordingly.

--- 232-canPlaceFlowers.py
+++ 232-canPlaceFlowers.py
@@ -5,4 +5,11 @@
 # Solution
 class Solution:
   def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    f = [0] + flowerbed + [0] # add 0 to the start and end of the flowerbed
+
+    for i in range(1, len(f) - 1): # loop through the flowerbed
+      if f[i - 1] == f[i] == f[i + 1] == 0: # if the current, previous, and next flowerbed are empty, plant a flower
+        f[i] = 1
+        n -= 1 # decrement the number of flowers to plant
     
+    return n <= 0 # return if the number of flowers to plant is less than or equal to 0
