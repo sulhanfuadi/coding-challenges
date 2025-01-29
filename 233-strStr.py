246b8683afae93ebb3831f37768677c00f104630
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-01-29 10:28:51 +0700

    feat: add initial setup for find the index of the first occurrence in a string problem
    Added initial setup for the 'Find the Index of the First Occurrence in a String' problem from LeetCode. The setup includes the class definition and method signature for the solution.

---
+++ 233-strStr.py
@@ -0,0 +1,8 @@
+# 28. Find the Index of the First Occurrence in a String
+# January 30, 2025
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+
+# Solution
+class Solution:
+  def strStr(self, haystack: str, needle: str) -> int:
+    
