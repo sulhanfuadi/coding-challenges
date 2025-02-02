596e385a3e9b4ed10f45631cc69a43456455a3a3
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-02 07:15:26 +0700

    feat: add initial setup for majority element problem
    Added initial setup for the 'Majority Element' problem from LeetCode. The setup includes the class definition and method signature for the solution.

---
+++ 234-majorityElement.py
@@ -0,0 +1,8 @@
+# 169. Majority Element
+# February 03, 2025
+# https://leetcode.com/problems/majority-element
+
+# Solution
+class Solution:
+  def majorityElement(self, nums: List[int]) -> int:
+    
