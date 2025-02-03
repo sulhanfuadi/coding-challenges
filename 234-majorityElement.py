c18c2afabae626891efb152eade3c3f24f0ffc8e
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-03 10:33:32 +0700

    feat: add solution for majority element problem using hash map
    Added a solution for the 'Majority Element' problem from LeetCode using a hash map. The solution counts the occurrences of each element and returns the element with the highest count.

--- 234-majorityElement.py
+++ 234-majorityElement.py
@@ -4,5 +4,15 @@
 
 # Solution
 class Solution:
-  def majorityElement(self, nums: List[int]) -> int:
+  # Approach 1: Hash Map
+  def majorityElementHashMap(self, nums: List[int]) -> int:
+    count = {}
+    result, maxCount = 0, 0
+
+    for num in nums:
+      count[num] = 1 + count.get(num, 0) # count the number of times each element appears, if it doesn't exist, set it to 0
+      if count[num] > maxCount:
+        maxCount = count[num]
+        result = num
     
+    return result
