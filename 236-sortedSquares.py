1ec318032aa7802ea04ac83316f0790d5bdc37ab
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-07 11:59:48 +0700

    feat: add initial setup for squares of a sorted array problem
    Added initial setup for the 'Squares of a Sorted Array' problem from LeetCode. The setup includes the class definition and method signature for the solution.

---
+++ 236-sortedSquares.py
@@ -0,0 +1,8 @@
+# 977. Squares of a Sorted Array
+# February 08, 2025
+# https://leetcode.com/problems/squares-of-a-sorted-array
+
+# Solution
+class Solution:
+  def sortedSquares(self, nums: List[int]) -> List[int]:
+    
