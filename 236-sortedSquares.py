b195e86d50158569961ba73b91d706ca3f9e9f15
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-08 13:21:13 +0700

    feat: implement solution for squares of a sorted array problem
    Completed the implementation for the 'Squares of a Sorted Array' problem from LeetCode. The solution uses two pointers to compare the squares of elements from both ends of the array and appends the larger square to the result list, which is then reversed before returning.

--- 236-sortedSquares.py
+++ 236-sortedSquares.py
@@ -5,4 +5,16 @@
 # Solution
 class Solution:
   def sortedSquares(self, nums: List[int]) -> List[int]:
-    
+    result = [] # create an empty list to store the result
+
+    l, r = 0, len(nums) - 1 # set the left and right pointers
+
+    while l <= r: 
+      if nums[l] ** 2 > nums[r] ** 2: # if the square of the left pointer is greater than the square of the right pointer
+        result.append(nums[l] ** 2) # append the square of the left pointer to the result
+        l += 1 # move the left pointer to the right
+      else:
+        result.append(nums[r] ** 2)
+        r -= 1
+
+    return result[::-1] # return the result in reverse order
