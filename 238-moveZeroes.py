1ce8525cfdebf95bbb701651dbea570089d429b8
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-12 12:09:26 +0700

    feat: implement solution for move zeroes problem
    Completed the implementation for the 'Move Zeroes' problem from LeetCode. The solution uses two pointers to move all non-zero elements to the front of the list while maintaining their relative order, and moves all zeroes to the end.

--- 238-moveZeroes.py
+++ 238-moveZeroes.py
@@ -5,4 +5,10 @@
 # Solution
 class Solution:
   def moveZeroes(self, nums: List[int]) -> None:
-    
+    l = 0 # left pointer, to keep track of the position of the first zero
+
+    # iterate through the list
+    for r in range(len(nums)):
+      if nums[r] != 0: # if the current element is not zero
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
