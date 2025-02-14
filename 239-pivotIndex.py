9392592981cecedf6e48a8ce4d2fccbb78ee22c9
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-14 14:22:36 +0700

    feat: implement solutions for find pivot index problem
    Completed the implementation for the 'Find Pivot Index' problem from LeetCode. The first solution calculates the left and right sums for each element and returns the pivot index if they are equal. The second solution optimizes the check by comparing twice the left sum plus the current element with the total sum.

--- 239-pivotIndex.py
+++ 239-pivotIndex.py
@@ -4,13 +4,26 @@
 
 # Solution
 class Solution:
+  # solution 1
   def pivotIndex(self, nums: List[int]) -> int:
     total = sum(nums) # sum of all elements
     left_sum = 0 # sum of elements to the left of the current element
 
     for i, num in enumerate(nums):
-      if left_sum == total - left_sum - num:
+      if left_sum == total - left_sum - num: # if left_sum == right_sum
         return i
       left_sum += num
     
+    return -1
+
+  # solution 2
+  def pivotIndex2(self, nums: List[int]) -> int:
+    total = sum(nums)
+    left_sum = 0
+
+    for i, num in enumerate(nums):
+      if left_sum * 2 + num == total: # if left_sum == right_sum
+        return i
+      left_sum += num
+
     return -1
