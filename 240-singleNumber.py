a3e57d5d1dcc878a86a9c1dd730dda7fede13d6f
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-16 13:45:30 +0700

    feat: implement solution for single number problem
    Implemented the solution for the 'Single Number' problem from LeetCode. The solution uses the XOR operator to cancel out duplicate numbers, leaving the unique number as the result.

--- 240-singleNumber.py
+++ 240-singleNumber.py
@@ -5,4 +5,9 @@
 # Solution
 class Solution:
   def singleNumber(self, nums: List[int]) -> int:
+    result = 0
+
+    for num in nums:
+      result ^= num # XOR operator, if the number is repeated, it will cancel out
     
+    return result # the only number that is not repeated will be left
