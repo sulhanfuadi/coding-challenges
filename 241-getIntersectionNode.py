39e1e53884c9101666b4e77f69e079f601c547d3
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-18 10:17:40 +0700

    feat: complete Intersection of Two Linked Lists solution
    Implemented the getIntersectionNode function in the Solution class in [241-getIntersectionNode.py](241-getIntersectionNode.py). The solution handles edge cases by checking for empty inputs and then uses a two-pointer technique to identify the intersection node by alternating pointers when reaching the end of a list.

--- 241-getIntersectionNode.py
+++ 241-getIntersectionNode.py
@@ -5,10 +5,23 @@
 # Solution
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+  def __init__(self, x):
+    self.val = x
+    self.next = None
 
 class Solution:
   def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    # edge cases, if either is empty, return None
+    if not headA or not headB:
+      return None
     
+    # get the length of both linked lists
+    pA = headA
+    pB = headB
+
+    while pA != pB:
+      pA = pA.next if pA else headB # if pA is None (end of list), set it to headB, else move to next node, same for pB
+      pB = pB.next if pB else headA
+    
+    # pA and pB will meet at the intersection point or at the end of the list, which is None
+    return pA
