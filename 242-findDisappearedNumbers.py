590e5fd62f895a2abfde212114c3a3859c6782d4
sulhanfuadi <sulhanfuadi096@gmail.com>
2025-02-19 11:18:22 +0700

    feat: add initial solution for find disappeared numbers problem
    Added the initial structure for solving the 'Find All Numbers Disappeared in an Array' problem in [242-findDisappearedNumbers.py](242-findDisappearedNumbers.py). This commit includes the problem header and the definition of the Solution class with the findDisappearedNumbers method signature.

---
+++ 242-findDisappearedNumbers.py
@@ -0,0 +1,8 @@
+# 448. Find All Numbers Disappeared in an Array
+# February 20, 2025
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+
+# Solution
+class Solution:
+  def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    
